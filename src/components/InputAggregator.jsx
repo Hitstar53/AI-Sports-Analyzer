880e5c2867cd885af639d5ab21bfb7d26935da2f
Valiant15 <vanshi.parmar15@gmail.com>
2024-04-03 21:06:20 +0530

    profile changes1

--- src/components/InputAggregator.jsx
+++ src/components/InputAggregator.jsx
@@ -4,18 +4,18 @@ const InputAggregator = () => {
   return (
     <header className={styles.inputAggregator}>
       <div className={styles.inputAggregatorChild} />
-      <div className={styles.outputProcessor}>
-        <img
-          className={styles.outputProcessorChild}
-          loading="lazy"
-          alt=""
-          src="/rectangle-411@2x.png"
-        />
-        <div className={styles.gamesenseaiWrapper}>
-          <h1 className={styles.gamesenseai}>GameSenseAi</h1>
+        <div className={styles.outputProcessor}>
+          <img
+            className={styles.outputProcessorChild}
+            loading="lazy"
+            alt=""
+            src="/rectangle-411@2x.png"
+          />
+          <div className={styles.gamesenseaiWrapper}>
+            <h1 className={styles.gamesenseai}>GameSenseAi</h1>
+          </div>
         </div>
-      </div>
-      <div className={styles.icbaselineMenuWrapper}>
+        <div className={styles.icbaselineMenuWrapper}>
         <img
           className={styles.icbaselineMenuIcon}
           loading="lazy"
